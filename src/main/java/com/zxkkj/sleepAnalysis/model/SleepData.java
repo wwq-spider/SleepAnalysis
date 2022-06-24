4baac65b765aba32d84cf94bb702babfbd56a4d2
heyq-666 <854180667@qq.com>
2022-06-24 17:20:36 +0800

    睡眠分析

--- src/main/java/com/zxkkj/sleepAnalysis/model/SleepData.java
+++ src/main/java/com/zxkkj/sleepAnalysis/model/SleepData.java
@@ -17,13 +17,15 @@ public class SleepData implements Serializable {
     private List<CsvRow> rowList = new ArrayList<>();
 
     //离床数据
-    private List<String[]> leaveDatas = new ArrayList<>();
+    private List<Integer[]> leaveDatas = new ArrayList<>();
 
     //在床数据
     private List<String[]> bedData = new ArrayList<>();
 
     //心率
     private List<String> hrData = new ArrayList<>();
+
+    private List<SleepInfo> sleepInfoList = new ArrayList<>();
 
     //呼吸率
 
