2e53edc0e7ef3d3257fa453aa285c24801a64b2b
heyq-666 <854180667@qq.com>
2022-09-19 14:39:23 +0800

    睡眠床垫最终交付版本

---
+++ src/main/java/com/zxkkj/sleepAnalysis/model/ShallowBreathInfo.java
@@ -0,0 +1,14 @@
+package com.zxkkj.sleepAnalysis.model;
+
+import lombok.Data;
+
+/**
+ * @author javabage
+ * @date 2022/8/19
+ */
+@Data
+public class ShallowBreathInfo {
+
+    private int shallowBreathStart;
+    private int shallowBreathEnd;
+}
